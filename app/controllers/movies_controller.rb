879de281c5187bba213b61e46868fedcae7dade7
Nikita Lukjanov <nlukjanov@gmail.com>
2018-03-23 16:59:48 +0000

    finished crud

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,4 +13,43 @@ class MoviesController < ApplicationController
     # default: render 'new' template
   end
 
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    id = params[:id]
+    @movie = Movie.find(id)
+  end
+
+  def update
+    id = params[:id]
+    @movie = Movie.find(id)
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+
+    respond_to do |client_wants|
+      client_wants.html { redirect_to movie_path(@movie) }
+      client_wants.xml { render xml: @movie.to_xml }
+    end
+
+
+  end
+
+  def destroy
+    id = params[:id]
+    @movie = Movie.find(id)
+    @movie.destroy
+    flash[:notice] = "Movie #{@movie.title} deleted."
+
+    redirect_to movies_path
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :rating, :release_date, :description)
+    end
+
 end
